2e5f2940594fe5b3563927c85592aa80b49c2102
Mr Footwork <pandau.ting@gmail.com>
2024-10-26 12:50:00 +0200

    iteration 9: centiseconds

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,51 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+	constructor() {
+		this.currentTime = 0;
+		this.intervalId = null;
+	}
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+	start(printTimeCallback) {
+		this.intervalId = setInterval(() => {
+			this.currentTime++;
+			if (printTimeCallback) printTimeCallback();
+		}, 10);
+	}
 
-  getMinutes() {
-    // ... your code goes here
-  }
+	getMinutes() {
+		return Math.floor(this.currentTime / 6000);
+	}
 
-  getSeconds() {
-    // ... your code goes here
-  }
+	getSeconds() {
+		return Math.floor((this.currentTime % 6000) / 100);
+	}
 
-  getCentiseconds() {
-    // ... your code goes here
-  }
+	getCentiseconds() {
+		return (this.currentTime % 6000) % 100;
+	}
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+	computeTwoDigitNumber(value) {
+		return value.toString().padStart(2, '0').slice(-2);
+	}
 
-  stop() {
-    // ... your code goes here
-  }
+	stop() {
+		clearInterval(this.intervalId);
+	}
 
-  reset() {
-    // ... your code goes here
-  }
+	reset() {
+		this.currentTime = 0;
+	}
 
-  split() {
-    // ... your code goes here
-  }
+	split() {
+		const minutes = this.computeTwoDigitNumber(
+			this.getMinutes(this.currentTime)
+		);
+		const seconds = this.computeTwoDigitNumber(
+			this.getSeconds(this.currentTime)
+		);
+		const centiSeconds = this.computeTwoDigitNumber(
+			this.getCentiseconds(this.currentTime)
+		);
+
+		return `${minutes}:${seconds}.${centiSeconds}`;
+	}
 }
